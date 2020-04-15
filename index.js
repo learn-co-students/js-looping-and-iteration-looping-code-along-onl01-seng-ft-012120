7cc34f4b59bf2da1029991b6bafb28fd115e2664
Patrick Grady <patickrgrady81@gmail.com>
2020-04-15 10:11:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function countDown(num) { 
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
+
+function writeCards(names, event) { 
+  let thanks = [];
+  for (let i = 0; i < names.length; i++) {
+    thanks.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return thanks;
+}
+
