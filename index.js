341554f8bcbfb43f728ef9ba8eab0563b038ca7d
kellypowers <kpowers.nj@gmail.com>
2020-04-17 18:43:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(array, occasion) {
+    let cards = []
+    for (let i = 0; i < array.length; i++) {
+        cards.push(`Thank you, ${array[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return cards
+}
+
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+
+function countDown(i) {
+    while (i > -1 ) {
+        console.log(i);
+        i--;
+    }
+}
+
+countDown(10)
